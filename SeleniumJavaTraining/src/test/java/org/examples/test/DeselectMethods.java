565c74e79d29176c82ecda721a370d6537371fab
Chetan Umesh Ingole <98551496+chetan-i9@users.noreply.github.com>
2022-10-07 23:57:34 +0530

    Add files via upload

---
+++ SeleniumJavaTraining/src/test/java/org/examples/test/DeselectMethods.java
@@ -0,0 +1,27 @@
+package org.examples.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DeselectMethods {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://only-testing-blog.blogspot.com/2014/01/textbox.html");
+
+        WebElement element= driver.findElement(By.name("FromLB"));
+        Select select=new Select(element);
+
+        select.selectByValue("USA");
+        select.selectByVisibleText("Russia");
+        select.selectByIndex(3);
+        select.deselectByValue("USA");
+        select.deselectByIndex(3);
+        select.deselectAll();
+    }
+
+}
