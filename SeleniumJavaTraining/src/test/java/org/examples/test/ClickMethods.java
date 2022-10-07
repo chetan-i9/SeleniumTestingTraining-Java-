b59c2c35ffbefb4d66d2412d1317bdad466a93db
Chetan Umesh Ingole <98551496+chetan-i9@users.noreply.github.com>
2022-10-07 23:54:42 +0530

    Update ClickMethods.java

--- SeleniumJavaTraining/src/test/java/org/examples/test/ClickMethods.java
+++ SeleniumJavaTraining/src/test/java/org/examples/test/ClickMethods.java
@@ -11,11 +11,18 @@ public class ClickMethods {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.saucedemo.com/");
+        //Implicit Wait
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.MILLISECONDS);
+        
         driver.findElement(By.id("user-name")).clear();
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).clear();
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+        
+        //Explicit Wait
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("item_4_title_link"))));
         driver.findElement(By.id("item_4_title_link")).click();
     }
 }
